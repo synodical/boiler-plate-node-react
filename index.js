d00fd62217f2e1a72bd8889416149ea38b2e186b
synodical <lena0323park@naver.com>
2022-02-13 14:50:43 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = 3000;
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+const { User } = require('./models/User');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://lena0323park:Yeah3756kl!@synodic.iz4ml.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     //useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify:false
 }).then(() => console.log('MongoDB is connected...'))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello world'));
+app.get('/', (req, res) => res.send('Hello world gg'));
 
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({
+            success: true
+        })
+    });
+})
 app.listen(port, () => console.log(`listening on port ${port}`));
 
 
